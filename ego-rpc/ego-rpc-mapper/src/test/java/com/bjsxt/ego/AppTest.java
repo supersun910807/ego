6f81675337d454e1c2b38c393879bffac512cdd8
supersun0807 <773755133@qq.com>
2019-11-09 12:31:22 +0800

    项目的创建

---
+++ ego-rpc/ego-rpc-mapper/src/test/java/com/bjsxt/ego/AppTest.java
@@ -0,0 +1,20 @@
+package com.bjsxt.ego;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
